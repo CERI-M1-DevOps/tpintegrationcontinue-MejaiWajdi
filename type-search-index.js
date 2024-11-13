d3dcd0938b7bf3556b8bfd1db8201ec3cb43fc8b
MejaiWajdi <MejaiWajdi@users.noreply.github.com>
2024-11-13 14:55:19 +0000

    deploy: 91ff6dae5c88ec572403acda3e9c5a7915570f0f

--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"liste","l":"ListeSimple"},{"p":"liste","l":"Noeud"}]
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"liste","l":"ListeSimple"},{"p":"liste","l":"Noeud"}];updateSearchResults();
